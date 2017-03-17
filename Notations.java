52acbdb3e9a53196c86605f03dd05c18d4d2a604
PavanTejaAnne <saipavanteja.anne@sjsu.edu>
2017-03-17 15:33:10 -0700

    Add files via upload

---
+++ Notations.java
@@ -0,0 +1,15 @@
+package com.umlparser;
+
+public class Notations {
+  	
+	public static String BOXOPEN = 	"[";		 	
+	public static String BOXCLOSE = "]";   		   				
+	public static String SPACE =   " ";   
+
+    //brackets
+	public static String SQRBRACKET = "[]";
+	public static String ANGLEBRKTOPEN = "<";
+	public static String ANGLEBRKTCLOSE = ">";
+	public static String ARRBRACKET = "(*)";
+     		
+}
