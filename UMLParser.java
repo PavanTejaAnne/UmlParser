989464023cccfb8e2a2db3242ea04cf7c723810b
PavanTejaAnne <saipavanteja.anne@sjsu.edu>
2017-04-19 22:57:38 -0700

    Update UMLParser.java

--- UMLParser.java
+++ UMLParser.java
@@ -1,7 +1,23 @@
 package com.umlparser;
 
+import java.io.FileInputStream;
 import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ModifierSet;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 
-public class UMLParser  {
+@SuppressWarnings("rawtypes")
+public class UMLParser extends VoidVisitorAdapter {	
 
-}	
+	private CompilationUnit cUnit;
+	private Model model = new Model();
+	
+	ClassDetails cDetails;
+	public void testRun(){
+		
+  }
+}
