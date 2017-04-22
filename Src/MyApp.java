1c0b93edab06c2022da8b443e021eef534558933
PavanTejaAnne <saipavanteja.anne@sjsu.edu>
2017-04-22 16:52:24 -0700

    Create MyApp.java

---
+++ Src/MyApp.java
@@ -0,0 +1,12 @@
+package com.umlparser;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class MyApp {
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+
+		}
+}
