cc9feacac5a5ceaf2745f754b908104bd0f680a0
PavanTejaAnne <saipavanteja.anne@sjsu.edu>
2017-04-22 16:51:32 -0700

    Create MethodDetails.java

---
+++ Src/MethodDetails.java
@@ -0,0 +1,37 @@
+package com.umlparser;
+
+public class MethodDetails {
+
+	
+	
+	private String name;
+	private char methodAccessModifier;
+	private String parameters;
+	private String methodReturnType;
+
+	public MethodDetails(String name, char methodAccessModifier, String parameters,
+			String methodReturnType) {
+		
+		this.name = name;
+		this.methodAccessModifier = methodAccessModifier;
+		this.parameters = parameters;
+		this.methodReturnType = methodReturnType;
+
+	}
+	
+	public String getname() {
+		return name;
+	}
+	
+	public char getMethodAccessModifier() {
+		return methodAccessModifier;
+	}
+
+	public String getMethodString(){
+		
+		String string = "";
+		string = methodAccessModifier + name + "(" + parameters +")" + methodReturnType + ";";
+		return string;
+	}
+
+}
