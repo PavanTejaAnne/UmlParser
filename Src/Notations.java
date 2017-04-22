36d671ec2669fa8f4b41fa9a15e2438b965bcdce
PavanTejaAnne <saipavanteja.anne@sjsu.edu>
2017-04-22 16:52:48 -0700

    Create Notations.java

---
+++ Src/Notations.java
@@ -0,0 +1,21 @@
+package com.umlparser;
+
+public class Notations {
+  	
+	public static String BOXOPEN = 	"[";		 	
+	public static String BOXCLOSE = "]";   		   				
+	public static String VERTSLASH  = "|";   				
+	public static String SPACE =   " "; 
+	public static String LITO  = "|"; 
+
+	public static String INTEGER = "int";
+	public static String STRING = "String"; 
+    	public static String VOID = "void";
+	
+    //brackets
+	public static String SQRBRACKET = "[]";
+	public static String ANGLEBRKTOPEN = "<";
+	public static String ANGLEBRKTCLOSE = ">";
+	public static String ARRBRACKET = "(*)";
+     		
+}
