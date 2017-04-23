71c03b1cc50efd7022eec80d863c5cb9b1ba860f
PavanTejaAnne <saipavanteja.anne@sjsu.edu>
2017-04-22 17:05:32 -0700

    Update Notations.java

--- Src/Notations.java
+++ Src/Notations.java
@@ -6,7 +6,6 @@ public class Notations {
 	public static String BOXCLOSE = "]";   		   				
 	public static String VERTSLASH  = "|";   				
 	public static String SPACE =   " "; 
-	public static String LITO  = "|"; 
 
 	public static String INTEGER = "int";
 	public static String STRING = "String"; 
@@ -17,5 +16,6 @@ public class Notations {
 	public static String ANGLEBRKTOPEN = "<";
 	public static String ANGLEBRKTCLOSE = ">";
 	public static String ARRBRACKET = "(*)";
+	public static String URL = "http://yuml.me/diagram/scruffy/class/";
      		
 }
